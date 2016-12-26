61ac0c65ccc7f7fc31d883eba0d504425ac391e2
G0BL1N <piraka282@gmail.com>
2016-12-26 08:13:15 +0300

    Add roll command

--- src/commands/useful.js
+++ src/commands/useful.js
@@ -48,6 +48,31 @@ module.exports = {
     },
     {
       prefix: prefix,
+      variants: ['roll', 'ролл'],
+      description: 'Выдаёт рандомный результат из указаного диапазона, ' +
+      'или от 1 до указанного числа.',
+      usage: prefix+'ролл 20 30',
+      async action(message, args) {
+
+        if(!args) {
+          let urfuckedgif =
+            'https://media.giphy.com/media/3oz8xVlyQAXwnD9C2A/giphy.gif';
+          message.channel.sendFile(urfuckedgif);
+          return;
+        }
+        let [min, max] = args.split(' ');
+        min = parseInt(min);
+        max = parseInt(max);
+        let result;
+        if(min && max)
+          result = Math.floor(Math.random() * (max - min + 1)) + min;
+        else if(min)
+          result = Math.floor(Math.random() * min) + 1;
+        message.channel.sendMessage(`\`${result}\``);
+      }
+    },
+    {
+      prefix: prefix,
       variants: ['purge'],
       description: 'Удаляет указанное количество сообщений из данного канала.',
       usage: prefix+'bulk 5',
