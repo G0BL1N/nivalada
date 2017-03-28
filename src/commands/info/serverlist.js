a3768210292136981fbe7a292de2ca76cc599773
G0BL1N <piraka282@gmail.com>
2017-03-28 22:44:48 +0300

    Add serverlist command

---
+++ src/commands/info/serverlist.js
@@ -0,0 +1,28 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+  variants: ['serverlist', 'serverls'],
+  description: 'Список серверов.',
+  usage: 'serverlist',
+  async action(message) {
+    const guilds = message.client.guilds.array();
+    const columnSize = Math.ceil(guilds.lenght/3);
+
+    const embed = new RichEmbed()
+      .setColor(0x36d148);
+    let str = '';
+    let counter = 0;
+    for(const guild of guilds) {
+      counter += 1;
+      str += `**${guild.name}`;
+      if(counter === columnSize || counter === guilds.length) {
+        embed.addField('', str, true);
+        counter = 0;
+        str = '';
+      } else {
+        str += '\n';
+      }
+    }
+    message.channel.sendEmbed(embed);
+  }
+}
