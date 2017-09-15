749ba7d026ecdf7e289c828f0cdb032b313a0282
Johnny <Johnny7A2@users.noreply.github.com>
2017-09-15 22:46:32 +0200

    Yes. Needed

---
+++ src/commands/communication/lenny.js
@@ -0,0 +1,8 @@
+module.exports = {
+  variants: ['lenny', 'le'],
+  description: '( ͡° ͜ʖ ͡° )',
+  usage: 'Russian description',
+  async action(message) {
+    message.channel.send('( ͡° ͜ʖ ͡° )');
+  }
+};
